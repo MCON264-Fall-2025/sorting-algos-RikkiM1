f4012651fdacf08556e38e5a810f38aa3058d7af
Rikkimann <rikkimann1234@gmail.com>
2026-01-05 21:35:56 -0500

    I think i did it right :)

--- src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
+++ src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
@@ -7,5 +7,13 @@ public class InsertionSort implements Sorter {
     @Override
     public <T> void sort(T[] a, Comparator<? super T> comp) {
         // TODO: implement insertion sort
+        for (int i = 1; i < a.length; i++) {
+            T key = a[i]; //key becomes a[0]
+            int j = i - 1; //j becomes one less then what number the loop is up to.
+            while (j >= 0 && comp.compare(a[j], key) > 0) {
+            a[j + 1] = a[j];
+            }
+        }
     }
 }
+
--- src/main/java/edu/touro/mcon264/sorting/MergeSort.java
+++ src/main/java/edu/touro/mcon264/sorting/MergeSort.java
@@ -5,7 +5,52 @@ import java.util.Comparator;
 public class MergeSort implements Sorter {
 
     @Override
+
     public <T> void sort(T[] a, Comparator<? super T> comp) {
-        // TODO: implement merge sort
+        if (a.length < 2) {
+            return;
+        }
+
+        int mid = a.length / 2;
+
+        // Split array
+        @SuppressWarnings("unchecked")
+        T[] left = (T[]) new Object[mid];
+        @SuppressWarnings("unchecked")
+        T[] right = (T[]) new Object[a.length - mid];
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = a[i];
+        }
+        for (int i = mid; i < a.length; i++) {
+            right[i - mid] = a[i];
+        }
+
+        // Recursively sort halves
+        sort(left, comp);
+        sort(right, comp);
+
+        // Merge halves
+        merge(a, left, right, comp);
+    }
+
+    private <T> void merge(T[] a, T[] left, T[] right, Comparator<? super T> comp) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (comp.compare(left[i], right[j]) <= 0) {
+                a[k++] = left[i++];
+            } else {
+                a[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            a[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            a[k++] = right[j++];
+        }
     }
 }
